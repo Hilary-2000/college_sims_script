e957fc59ddc38e1ca9e312fd0551ccbc42894b05
Hillary Ngige <hilaryme45@gmail.com>
2024-02-26 12:01:07 +0300

    Init College Update Script

---
+++ README.md
@@ -0,0 +1,1 @@
+# college_sims_script
---
+++ upgrades_2/file_1.php
@@ -0,0 +1,20 @@
+<?php
+    $json_data = '[
+        {
+            "download_file": "/opt/lampp/htdocs/college_sims_script/file_1.txt",
+            "replace_file": "/opt/lampp/htdocs/college_sims_script/upgrades/file_1.txt"
+        }
+    ]';
+    $json_data_1 = '[
+        {
+            "download_file": "/opt/lampp/htdocs/college_sims_script/file_1.txt",
+            "replace_file": "/opt/lampp/htdocs/college_sims_script/upgrades/file_1.txt"
+        }
+    ]';
+    $hillary_me_45 = '[
+        {
+            "download_file": "/opt/lampp/htdocs/college_sims_script/file_1.txt",
+            "replace_file": "/opt/lampp/htdocs/college_sims_script/upgrades/file_1.txt"
+        }
+    ]';
+?>
